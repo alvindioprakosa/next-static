2c8cf588eeaa355cc6fe4fa079ae3e7251887336
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-14 10:16:08 +0700

    Update Text

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Homepage</h1>
       <p>Hello World! This is the Home page</p>
       <p>
-        Visit the <Link href="/about">About</Link> page.
+        Visit in <Link href="/about">About</Link> page.
       </p>
     </div>
   );
